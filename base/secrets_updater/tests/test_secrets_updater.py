ae92d0d713047537f17e0900b3f3eb3fcb1dfa40
kiran-karandikar <kkarandikar@bmiaudit.com>
2022-08-11 17:24:14 -0400

    Added Test stub

--- base/secrets_updater/tests/test_secrets_updater.py
+++ base/secrets_updater/tests/test_secrets_updater.py
@@ -1,3 +1,11 @@
 """
 # todo.
 """
+def area_of_rectangle(width, height):
+    area = width * height
+    return area
+
+
+def test_area():
+    output = area_of_rectangle(2, 5)
+    assert output == 10
