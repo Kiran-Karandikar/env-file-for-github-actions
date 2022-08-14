81512377d465f6788d4dc13433b96631dedfa8b7
kiran-karandikar <kkarandikar@bmiaudit.com>
2022-08-14 13:39:35 -0400

    Test commit.

--- base/compose/local-env/actions.sh
+++ base/compose/local-env/actions.sh
@@ -8,7 +8,8 @@ mkdir -p .envs/.local
 
 # todo: create sample note here
 
-touch .envs/.local/.sample
-echo "SECRET_1=${{ secrets.SECRET_1 }}" >> .envs/.local/.sample
-echo "SECRET_2=${{ secrets.SECRET_2 }}" >> .envs/.local/.sample
-cat .envs/.local/.sample
+#touch .envs/.local/.sample
+#echo SECRET_1='${{ secrets.SECRET_1 }}' >> .envs/.local/.sample
+## shellcheck disable=SC2016
+#echo SECRET_2='${{ secrets.SECRET_2 }}' >> .envs/.local/.sample
+#cat .envs/.local/.sample
