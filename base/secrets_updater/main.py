637521d74ca9a724ccdde323d5a6136f20caf663
kiran-karandikar <kkarandikar@bmiaudit.com>
2022-08-18 14:14:08 -0400

    fix(Global): Minor bug fixes

--- base/secrets_updater/main.py
+++ base/secrets_updater/main.py
@@ -9,7 +9,7 @@ import os
 import sys
 
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # Project Libraries
 from secrets_updater.github import GitHubActions  # noqa: E402
